36a8830920b3abc298827c47fe4234f74679f243
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-04-16 10:23:10 -0400

    feature: logic completed with hardcoded date

--- lib/new_year_counter.ex
+++ lib/new_year_counter.ex
@@ -7,6 +7,15 @@ defmodule NewYearCounter do
   end
 
   def main do
+    time = DateTime.new!(Date.new!(2025, 1, 1), Time.new!(0,0,0,0), "Etc/UTC")
+    counter = DateTime.diff(time, DateTime.utc_now())
 
+    days = div(counter, 86400)
+    hours = div(rem(counter, 86400), 60 * 60)
+    minutes = div(rem(counter, 3600), 60)
+    seconds = rem(counter, 60)
+
+    IO.puts("Time until new year:")
+    IO.puts("#{days} days, #{hours} hours, #{minutes} minutes, #{seconds} seconds")
   end
 end
